791c405f577a73f0971f241963aef29505636431
MV Admin <mvadmin@MVCC-MACMINI-72.local>
2019-05-15 18:33:56 -0700

    commit

--- index.js
+++ index.js
@@ -1,43 +1,54 @@
-var objects;
+var screens;
+var currentScreen;
 
 function setup() {
 	var canvas = createCanvas(640, 640);
 	canvas.parent('mycanvas');
 	background(0, 255, 0);
 
-	objects = [];
-	mainMenu();
+	screens = [];
+	setupMainMenu();
 }
 
 function draw() {
-	for (let obj of objects) {
-		obj.drawObj();
+	for (let screen of screens) {
+		screen.drawScreen();
 	}
 }
 
 window.addEventListener('click', function(e) {
-	for (let obj of objects) {
-		if (obj.mouseOn(e.x, e.y)) {
-			if (obj.name == 'playButton') {
-				playButtonClicked();
-			} else if (obj.name == 'mainMenu') {
-				mainMenuButtonClicked();
-			} else {
-				obj.clicked();
+	for (let screen of screens) {
+		if (screen.visible) {
+			for (let obj of screen.list) {
+				if (obj.mouseOn(e.x, e.y)) {
+					if (obj.name == 'playButton') {
+						playButtonClicked();
+					} else if (obj.name == 'mainMenu') {
+						mainMenuButtonClicked();
+					} else {
+						obj.clicked();
+					}
+				}
 			}
 		}
 	}
+
+	
 })
 
-function mainMenu() {
+function setupMainMenu() {
+	screens.push(new Screen('mainMenu'));
+
 	titleText = new Text('titleText', 100, 100, color(0, 0, 255), 'Random Game Name', 50, true);
-	objects.push(titleText);
+	screens[0].addObj(titleText);
 
 	playButton = new Button('playButton', 250, 270, 175, 75, color(0, 0, 255), color(255, 0, 0), 'Play', 32, true);
-	objects.push(playButton);
+	screens[0].addObj(playButton);
 
 	mainMenuButton = new Button('mainMenu', 237.5, 350, 200, 75, color(0, 0, 255), color(255, 0, 0), 'Main Menu', 32, true);
-	objects.push(mainMenuButton);
+	screens[0].addObj(mainMenuButton);
+
+	currentScreen = screen[0];
 }
 
 function playButtonClicked() {
@@ -46,4 +57,32 @@ function playButtonClicked() {
 
 function mainMenuButtonClicked() {
 	console.log('The main menu button was clicked');
+}
+
+function binaryFindScreen(screenName) {
+	let min = 0;
+	let max = screens.length - 1;
+	let spot;
+
+	while (min != max) {
+		spot = (min + max) / 2;
+
+		if (screenName.localeCompare(screens[spot].name) == 0) {
+			break;
+		} else if (screenName.localeCompare(screens[spot].name) < 0) {
+			max = spot;
+		} else {
+			min = spot;
+		}
+	}
+
+	return screens[spot];
+}
+
+function addScreen(screen) {
+	for (let i = 0; i < screens.length; i++) {
+		if (screen.name.localeCompare(screens[i].name) < 0) {
+			screens.splice(i, 0, screen);
+		}
+	}
 }
--- screen.js
+++ screen.js
@@ -1,12 +1,21 @@
 class Screen {
-	constructor(screenName) {
+	constructor(screenName, visible) {
 		this.name = screenName;
+		this.visible = visible || true;
 		this.list = [];
 	}
 
 	drawScreen() {
 		for (let obj of this.list) {
-			
+			obj.drawObj();
 		}
+	}
+
+	addObj(obj) {
+		this.list.push(obj);
+	}
+
+	removeObj(obj) {
+		this.list.filter(x => x != obj);
 	}
 }
